66de88518613a4e59680fc93c7d6e4cde6b09c0e
Rishad Omar <rishad@aux.co.za>
2023-04-15 16:59:53 +0200

    First push

---
+++ src/FirebaseConfig.ts
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase/app';
+
+const config = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID
+};
+
+if (!firebase.getApps().length) {
+    firebase.initializeApp(config);
+}
+
+export default firebase;
